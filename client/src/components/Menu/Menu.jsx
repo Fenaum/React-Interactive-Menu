d134de9d8c0bf0c1de2cbb83430d347f3f20a6c5
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2023-09-20 20:20:56 -0700

    added components and set up Routers

---
+++ client/src/components/Menu/Menu.jsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+    return (
+        <div>
+            this is a menu
+        </div>
+    )
+}
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+    return (
+        <div>
+            <div>
+                <h3>The Menu</h3>
+            </div>
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
