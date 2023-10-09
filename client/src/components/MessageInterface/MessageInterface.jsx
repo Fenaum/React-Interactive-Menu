ac8f832439919dd8a0f6fd8a1fc83c8a16d431a2
Raymond <hnguyen4497@outlook.com>
2023-10-08 19:39:12 -0700

    added functions to message interface

--- client/src/components/MessageInterface/MessageInterface.jsx
+++ client/src/components/MessageInterface/MessageInterface.jsx
@@ -1,13 +1,47 @@
-import chat from "../../assets/images/message/chat.svg"
-import "./MessageInterface.css"
+import React, { useState } from "react";
+import chat from "../../assets/images/message/chat.svg";
+import "./MessageInterface.css";
 
 export default function MessageInterface() {
-    return (
-        <div>
-            <div></div>
-            <button className="chat-button" >
-                <img className="chat-icon" src={chat} alt="chat-icon" />
-            </button>
+  const [showChat, setShowChat] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleButtonClick = () => {
+    setShowChat(!showChat);
+  };
+
+  const submitMessage = (e) => {
+    e.preventDefault();
+    if (message.trim() !== "") {
+      setMessages([...messages, message]);
+      setMessage("");
+    }
+  };
+
+  return (
+    <div>
+      <div></div>
+      <button className="chat-button" onClick={handleButtonClick}>
+        <img className="chat-icon" src={chat} alt="chat-icon" />
+      </button>
+      {showChat && (
+        <div className="chat-interface">
+          <div className="chat-messages">
+            {messages.map((msg, index) => (
+              <p key={index}>{msg}</p>
+            ))}
+          </div>
+          <form onSubmit={submitMessage}>
+            <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Type a message"
+            />
+            <button type="submit">Send</button>
+          </form>
         </div>
-    )
+      )}
+    </div>
+  );
 }
